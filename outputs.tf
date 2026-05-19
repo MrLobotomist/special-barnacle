bdbeed832edb538bb6e60503f0fbd3e16ae59bf0
MrLobotomist <39457028+MrLobotomist@users.noreply.github.com>
2026-05-19 23:49:06 +0500

    fix: add container names to outputs

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,19 @@ output "app_url" {
   value       = "http://localhost:${var.app_port}"
 }
 
+output "app_container_name" {
+  description = "App container name"
+  value       = docker_container.app.name
+}
+
 output "app_container_id" {
   description = "App container ID"
   value       = docker_container.app.id
+}
+
+output "db_container_name" {
+  description = "PostgreSQL container name"
+  value       = docker_container.db.name
 }
 
 output "db_container_id" {
